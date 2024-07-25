4d773da1d6f7803c25dacd2f4671359f1b60440a
Ansh Kumar <115077985+ansh3108@users.noreply.github.com>
2024-07-25 17:11:36 +0530

    Update raycasting.py

--- raycasting.py
+++ raycasting.py
@@ -9,7 +9,29 @@ class RayCasting:
         self.objects_to_render = []
         self.textures = self.game.object_renderer.wall_textures
 
-def ray_cast(self):
+    def get_objects_to_render(self):
+        self.objects_to_render = []
+        for ray, values in enumerate(self.ray_casting_result):
+            depth, proj_height, texture, offset = values
+
+            if proj_height < HEIGHT:
+                wall_column = self.textures[texture].subsurface(
+                    offset * (TEXTURE_SIZE - SCALE), 0, SCALE, TEXTURE_SIZE
+                )
+                wall_column = pg.transform.scale(wall_column, (SCALE, proj_height))
+                wall_pos = (ray * SCALE, HALF_HEIGHT - proj_height // 2)
+            else:
+                texture_height = TEXTURE_SIZE * HEIGHT / proj_height
+                wall_column = self.textures[texture].subsurface(
+                    offset * (TEXTURE_SIZE - SCALE), HALF_TEXTURE_SIZE - texture_height // 2,
+                    SCALE, texture_height
+                )
+                wall_column = pg.transform.scale(wall_column, (SCALE, HEIGHT))
+                wall_pos = (ray * SCALE, 0)
+
+            self.objects_to_render.append((depth, wall_column, wall_pos))
+
+    def ray_cast(self):
         self.ray_casting_result = []
         texture_vert, texture_hor = 1, 1
         ox, oy = self.game.player.pos
@@ -77,6 +99,6 @@ def ray_cast(self):
 
             ray_angle += DELTA_ANGLE
 
-def update(self):
+    def update(self):
         self.ray_cast()
         self.get_objects_to_render()
